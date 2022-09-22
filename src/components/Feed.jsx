2bf3de4f1a53ff4e80df9a07f4d4c17886f98198
yaser <ddemety372@gmail.com>
2022-09-22 17:33:40 +0300

    videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,65 @@
-import React from 'react'
-import {useState , useEffect} from "react"
-import {Box , Typography , Stack } from "@mui/material"
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import SideBar from "./SideBar";
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack
-    sx={{flexDirection : {
-      sx : "column" , md : "row"
-    }}}
-    >Feed</Stack>
-  )
-}
+      sx={{
+        flexDirection: {
+          sx: "column",
+          md: "row",
+        },
+        backgroundColor: "black",
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
-export default Feed
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh ,  flex : 2" }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/components/Videos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Stack , Box} from "@mui/material"
+
+const Videos = ({videos}) => {
+    // console.log(videos)
+  return (
+    <div>Videos</div>
+  )
+}
+
+export default Videos
